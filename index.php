409c3c691bc1283eeee4bcfbdf36771f87960b04
leloucha <33195631+leloucha@users.noreply.github.com>
2024-11-28 23:13:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,5 @@ $programingLanguages = ['php', 'python', 'c++', 'java', 'python'];
 foreach ($programingLanguages as $key => $languages) {
     echo $key . ':' . $languages . '<br />';
 }
+echo print ('ahihi');
 ?>
